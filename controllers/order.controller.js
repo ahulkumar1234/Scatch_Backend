3a629fe5e9a6b620e6a011e5fa6d0c02adf04f0e
Rahul Kumar <rahulkumar8340527941@gmail.com>
2025-12-28 13:46:37 +0530

    bug fixed

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -67,7 +67,7 @@ const createOrder = async (req, res) => {
 
 
         //Cart clear after Order succesfully!
-        await Cartmodel.deleteMany({ userId: userId.id });
+        await Cartmodel.deleteMany({ userId: req.user.id });
 
 
         res.status(201).json({
